fe05863248a5f293e8acedb4fed1af52b11347d1
ElyPoli <itsos.poli@gmail.com>
2023-08-31 15:02:34 +0200

    created files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+
+export default {
+    components: {
+        TheHeader,
+        TheFooter,
+    }
+}
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <main>
+    <p>Titolo</p>
+    <h1>Titolo</h1>
+  </main>
+  <TheFooter></TheFooter>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <footer>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+</style>

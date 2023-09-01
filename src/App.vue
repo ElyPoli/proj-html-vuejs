8a1b81683a02d92ba5b52f6dfdf963390e3082be
ElyPoli <itsos.poli@gmail.com>
2023-09-01 13:26:16 +0200

    added card menù

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
+import MainCards from "./components/MainCards.vue";
 
 export default {
     components: {
         TheHeader,
         TheFooter,
+        MainCards,
     }
 }
 </script>
 
 <template>
   <TheHeader></TheHeader>
-  <main>
-    <p>Titolo</p>
-    <h1>Titolo</h1>
+  <main class="pt-5 pb-5">
+    <MainCards></MainCards>
   </main>
   <TheFooter></TheFooter>
 </template>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    data() {
+        return {
+            cardMenu: [
+                {
+                    name: "Languages",
+                    img: "card-menu-1.png",
+                },
+                {
+                    name: "Software",
+                    img: "card-menu-2.png",
+                },
+                {
+                    name: "Business",
+                    img: "card-menu-3.png",
+                },
+                {
+                    name: "Chemistry",
+                    img: "card-menu-4.png",
+                },
+                {
+                    name: "Science",
+                    img: "card-menu-5.png",
+                },
+                {
+                    name: "DIY&Craft",
+                    img: "card-menu-6.png",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container pt-2">
+        <div class="row row-cols-lg-6 row-cols-md-3 row-cols-sm-2 row-cols-1">
+            <div class="col" v-for="(card, i) in cardMenu" :key="i">
+                <div class="my-card-menu">
+                    <div>
+                        <img v-bind:src="`/public/img/${card.img}`" v-bind:alt="card.name">
+                        <p>{{ card.name }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.my-card-menu {
+    background-color: $background-light;
+    height: 200px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    p {
+        margin: 0;
+        padding-top: 1.5rem;
+        font-weight: bold;
+    }
+
+    img {
+        transition: transform .5s;
+    }
+
+    &:hover img {
+        transform: translateY(-8%);
+    }
+}
+</style>

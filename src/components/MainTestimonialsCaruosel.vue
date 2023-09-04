2c8fabdea3d6fa944aec0b2e279aaa369e449907
ElyPoli <itsos.poli@gmail.com>
2023-09-04 10:06:35 +0200

    update section testimonials (added slider)

--- src/components/MainTestimonialsCaruosel.vue
+++ src/components/MainTestimonialsCaruosel.vue
@@ -1,25 +1,52 @@
 <script>
 export default {
+    data() {
+        return {
+            testimonialsList: [
+                {
+                    name: "Molly Simon",
+                    text: `“Mroin gravida nibh vel veliauctor aliquenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsutis sem nibh id elit. Duis sed odio sit amet nivulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus”`,
+                    occupation: "Teacher",
+                    img: "testimonials-img-1.png",
+                },
+                {
+                    name: "Joan Collins",
+                    text: `“Mroin gravida nibh vel veliauctor aliquenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsutis sem nibh id elit. Duis sed odio sit amet nivulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus”`,
+                    occupation: "Student",
+                    img: "testimonials-img-2.png",
+                },
+                {
+                    name: "Virginia Foster",
+                    text: `“Mroin gravida nibh vel veliauctor aliquenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsutis sem nibh id elit. Duis sed odio sit amet nivulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus”`,
+                    occupation: "Student",
+                    img: "testimonials-img-3.png",
+                },
+            ],
+            currentSlide: 0,
+        }
+    },
+    methods: {
+        btnChangeSlide(index) {
+            this.currentSlide = index;
+        }
+    },
 }
 </script>
 
 <template>
     <div class="testimonials-container d-flex text-center justify-content-center align-items-center">
         <div class="container w-50">
-            <img src="/public/img/testimonials-img-1.png" alt="testimonial">
-            <p class="pb-2 pt-4">
-                “Mroin gravida nibh vel veliauctor aliquenean sollicitudin, lorem quis bibendum auctor, nisi elit
-                consequat
-                ipsutis sem nibh id elit. Duis sed odio sit amet nivulputate cursus a sit amet mauris. Morbi accumsan
-                ipsum
-                velit. Nam nec tellus”
-            </p>
-            <h5 class="m-0 pt-2 pb-2">Molly Simon</h5>
-            <p class="m-0 pt-2 pb-2 text-uppercase fw-bold">Teacher</p>
+            <div v-for="(testimonial, i) in testimonialsList" :key="i"
+                v-bind:class="(currentSlide === i ? 'testimonials-active testimonials-card' : 'testimonials-card')">
+                <img v-bind:src="`/public/img/${testimonial.img}`" v-bind:alt="testimonial.name">
+                <p class="pb-2 pt-4">{{ testimonial.text }}</p>
+                <h5 class="m-0 pt-2 pb-2">{{ testimonial.name }}</h5>
+                <p class="m-0 pt-2 pb-2 text-uppercase fw-bold">{{ testimonial.occupation }}</p>
+            </div>
             <div class="pt-3">
-                <i class="fa-solid fa-circle pe-2"></i>
-                <i class="fa-solid fa-circle pe-2"></i>
-                <i class="fa-solid fa-circle"></i>
+                <i v-for="(testimonial, i) in testimonialsList" :key="i" @click="btnChangeSlide(i)"
+                    v-bind:class="(currentSlide === i ? 'fa-solid fa-circle pe-2 testimonials-btn-active' : 'fa-solid fa-circle pe-2')">
+                </i>
             </div>
         </div>
     </div>
@@ -49,10 +76,21 @@ export default {
         &:active {
             opacity: 1;
         }
+    }
 
-        &:first-of-type {
-            opacity: 1;
-        }
+    i.testimonials-btn-active {
+        opacity: 1;
+    }
+
+    .testimonials-card {
+        display: none;
+        opacity: 0;
+        transition: opacity .5s;
+    }
+
+    .testimonials-card.testimonials-active {
+        display: inline-block;
+        opacity: 1;
     }
 }
 </style>
